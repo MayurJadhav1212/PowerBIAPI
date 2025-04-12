9e25711542e2f130d72a7902012482781bdf94cb
MayurJadhav1212 <MayurJadhav1212>
2025-04-12 21:27:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+
+const app = express();
+app.use(cors());
+
+// PostgreSQL connection setup
+const pool = new Pool({
+  user: 'postgres',
+  host: '51.20.188.162',
+  database: 'postgres',
+  password: 'SssvM@379738',
+  port: 5432,
+});
+
+// Simple API to fetch borrowers data
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`API running at http://localhost:${PORT}`);
+});
+
+
